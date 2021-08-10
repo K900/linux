fd42546bcca635744f28779b7fd69d00072ccc2a
Ben Greear <greearb@candelatech.com>
2021-08-09 19:16:12 -0700

    wifi: Support disabling 160Mhz bandwidths in station mode.
    For testing purposes.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -13297,6 +13297,8 @@ static int nl80211_associate(struct sk_buff *skb, struct genl_info *info)
 			}
 			if (cai->flags & CT_DISABLE_TWT)
 				req.flags |= ASSOC_REQ_DISABLE_TWT;
+			if (cai->flags & CT_DISABLE_160MHZ)
+				req.flags |= ASSOC_REQ_DISABLE_160;
 		}
 	}
 skip_ct_priv:
@@ -14201,6 +14203,8 @@ static int nl80211_connect(struct sk_buff *skb, struct genl_info *info)
 			}
 			if (cai->flags & CT_DISABLE_TWT)
 				connect.flags |= ASSOC_REQ_DISABLE_TWT;
+			if (cai->flags & CT_DISABLE_160MHZ)
+				connect.flags |= ASSOC_REQ_DISABLE_160;
 		}
 	}
 	skip_ct_priv:
