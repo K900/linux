b53b8a6f559e26a94da4596c0d3ec8dd220f0686
Ben Greear <greearb@candelatech.com>
2022-07-27 15:48:57 -0700

    wifi: mt76: mt7915: use nss for calculating rx-chains
    When group-5 is enabled, we can find the actuall NSS used.
    In that case, update the rx chains info so that the upper
    stack can give better idea of actual antenna usage.
    
    In case of stbc, the hardware already double nss, but rssi
    is not reported properly (it is '17') for the secondary chains.
    So, decrease nss in the status msg so that mac80211 does not try
    to average the invalid RSSI.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt76_connac_mac.c
+++ drivers/net/wireless/mediatek/mt76/mt76_connac_mac.c
@@ -1056,10 +1056,10 @@ EXPORT_SYMBOL_GPL(mt76_connac2_reverse_frag0_hdr_trans);
 int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 				  struct mt76_rx_status *status,
 				  struct ieee80211_supported_band *sband,
-				  __le32 *rxv, u8 *mode)
+				  __le32 *rxv, u8 *mode, u8 *nss)
 {
 	u32 v0, v2;
-	u8 stbc, gi, bw, dcm, nss;
+	u8 stbc, gi, bw, dcm;
 	int i, idx;
 	bool cck = false;
 
@@ -1067,7 +1067,7 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 	v2 = le32_to_cpu(rxv[2]);
 
 	idx = i = FIELD_GET(MT_PRXV_TX_RATE, v0);
-	nss = FIELD_GET(MT_PRXV_NSTS, v0) + 1;
+	*nss = FIELD_GET(MT_PRXV_NSTS, v0) + 1;
 
 	if (!is_mt7915(dev)) {
 		stbc = FIELD_GET(MT_PRXV_HT_STBC, v0);
@@ -1087,8 +1087,8 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 	}
 
 	/* the hardware reports NSTS; report the data NSS for STBC frames */
-	if (stbc && nss > 1)
-		nss >>= 1;
+	if (stbc && *nss > 1)
+		*nss >>= 1;
 
 	switch (*mode) {
 	case MT_PHY_TYPE_CCK:
@@ -1096,17 +1096,21 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 		fallthrough;
 	case MT_PHY_TYPE_OFDM:
 		i = mt76_get_rate(dev, sband, i, cck);
+		if (stbc)
+			*nss = 2;
+		else
+			*nss = 1;
 		break;
 	case MT_PHY_TYPE_HT_GF:
 	case MT_PHY_TYPE_HT:
 		status->encoding = RX_ENC_HT;
+		*nss = i / 8 + 1;
 		if (gi)
 			status->enc_flags |= RX_ENC_FLAG_SHORT_GI;
 		if (i > 31)
 			return -EINVAL;
 		break;
 	case MT_PHY_TYPE_VHT:
-		status->nss = nss;
 		status->encoding = RX_ENC_VHT;
 		if (gi)
 			status->enc_flags |= RX_ENC_FLAG_SHORT_GI;
@@ -1117,7 +1121,6 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 	case MT_PHY_TYPE_HE_SU:
 	case MT_PHY_TYPE_HE_EXT_SU:
 	case MT_PHY_TYPE_HE_TB:
-		status->nss = nss;
 		status->encoding = RX_ENC_HE;
 		i &= GENMASK(3, 0);
 
@@ -1157,6 +1160,8 @@ int mt76_connac2_mac_fill_rx_rate(struct mt76_dev *dev,
 	status->enc_flags |= RX_ENC_FLAG_STBC_MASK * stbc;
 	if (*mode < MT_PHY_TYPE_HE_SU && gi)
 		status->enc_flags |= RX_ENC_FLAG_SHORT_GI;
+
+	status->nss = *nss;
 
 	return 0;
 }
