bcb80850ecbe4ff249026a3f0a3988b6990996bb
Ben Greear <greearb@candelatech.com>
2024-08-09 17:06:07 -0700

    mt76: Add verbose tx debugging
    Enabled by module parameter.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/dma.c
+++ drivers/net/wireless/mediatek/mt76/dma.c
@@ -329,6 +329,9 @@ mt76_dma_add_buf(struct mt76_dev *dev, struct mt76_queue *q,
 	int i, idx = -1;
 	u32 ctrl, next;
 
+	mtk_dbg(dev, TXV, "mt76-dma-add-buf, txwi: %p nbufs: %d q->queued: %d\n",
+		txwi, nbufs, q->queued);
+
 	if (txwi) {
 		q->entry[q->head].txwi = DMA_DUMMY_DATA;
 		q->entry[q->head].skip_buf0 = true;
@@ -385,6 +388,9 @@ mt76_dma_add_buf(struct mt76_dev *dev, struct mt76_queue *q,
 	q->entry[idx].skb = skb;
 	q->entry[idx].wcid = 0xffff;
 
+	mtk_dbg(dev, TXV, "mt76-dma-add-buf, at end, idx: %d  skb: %p  txwi: %p  q->queued: %d\n",
+		idx, skb, txwi, q->queued);
+
 	return idx;
 }
 
@@ -413,6 +419,8 @@ static void
 mt76_dma_kick_queue(struct mt76_dev *dev, struct mt76_queue *q)
 {
 	wmb();
+	mtk_dbg(dev, TXV, "mt76-dma-kick-queue, q: %p\n",
+		q);
 	if (mt76_queue_is_emi(q))
 		*q->emi_cpu_idx = cpu_to_le16(q->head);
 	else
@@ -425,6 +433,9 @@ mt76_dma_tx_cleanup(struct mt76_dev *dev, struct mt76_queue *q, bool flush)
 	struct mt76_queue_entry entry;
 	int last;
 
+	mtk_dbg(dev, TXV, "mt76-dma-tx-cleanup, q: %p flush: %d\n",
+		q, flush);
+
 	if (!q || !q->ndesc)
 		return;
 
@@ -434,6 +445,8 @@ mt76_dma_tx_cleanup(struct mt76_dev *dev, struct mt76_queue *q, bool flush)
 	else
 		last = mt76_dma_read_dma_idx(q, -1);
 
+	mtk_dbg(dev, TXV, "mt76-dma-tx-cleanup, queued: %d  last: 0x%x  q->tail: 0x%x\n",
+		q->queued, last, q->tail);
 	while (q->queued > 0 && q->tail != last) {
 		mt76_dma_tx_cleanup_idx(dev, q, q->tail, &entry);
 		mt76_npu_txdesc_cleanup(q, q->tail);
@@ -570,6 +583,9 @@ mt76_dma_dequeue(struct mt76_dev *dev, struct mt76_queue *q, bool flush,
 		 int *len, u32 *info, bool *more, bool *drop)
 {
 	int idx = q->tail;
+
+	mtk_dbg(dev, RXV, "mt76-dma-dequeue, tail-idx: %d queued: %d\n",
+		idx, q->queued);
 
 	*more = false;
 	if (!q->queued)
@@ -794,6 +810,8 @@ free_skb:
 	spin_lock_bh(&dev->rx_lock);
 	ieee80211_tx_status_ext(hw, &status);
 	spin_unlock_bh(&dev->rx_lock);
+
+	mtk_dbg(dev, TXV, "mt76-dma-tx-queue-skb failed, ret: %d\n", ret);
 
 	return ret;
 }
