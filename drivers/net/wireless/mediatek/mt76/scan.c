198de345589c1d0e078089276a7f6b427558e0a2
Rory Little <rory@candelatech.com>
2025-11-13 11:06:40 -0800

    wifi: mt76: Check for NULL link info before scanning.
    This occassionally gets called before the mlink is initialized, so the
    WCID field is NULL. Easy fix is to punt the issue and rescan later.
    
    Signed-off-by: Rory Little <rory@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/scan.c
+++ drivers/net/wireless/mediatek/mt76/scan.c
@@ -58,6 +58,12 @@ mt76_scan_send_probe(struct mt76_dev *dev, struct cfg80211_ssid *ssid)
 	u8 link_id;
 	struct ieee80211_hdr *hdr;
 
+	if (WARN_ON_ONCE(!mvif))
+		return;
+
+	if (WARN_ON_ONCE(!mvif->wcid))
+		return;
+
 	skb = ieee80211_probereq_get(phy->hw, vif->addr, ssid->ssid,
 				     ssid->ssid_len, req->ie_len);
 	if (!skb)
