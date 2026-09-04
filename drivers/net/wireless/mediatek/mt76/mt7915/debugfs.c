greearb/linux-ct-6.17

wifi: mt76: mt7915: Add vif-level set_rate_override debugfs file
This is tied as a feature to VIFs, fundamentally. Therefore, it should
exist at a VIF level. This saves us from needing to pass a VIF to the
file for lookup.

Also fix a bug for AP VIFs, where the testmode config was not being
correctly propogated to new stations.

Signed-off-by: Rory Little <rory@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
@@ -751,12 +751,14 @@ mt7915_fw_util_wa_show(struct seq_file *file, void *data)
 
 DEFINE_SHOW_ATTRIBUTE(mt7915_fw_util_wa);
 
+/* DEPRECATED: See vif_set_rate_override */
 struct mt7915_txo_worker_info {
 	char *buf;
 	int sofar;
 	int size;
 };
 
+/* DEPRECATED: See vif_set_rate_override */
 static void mt7915_txo_worker(void *wi_data, struct ieee80211_sta *sta)
 {
 	struct mt7915_txo_worker_info *wi = wi_data;
@@ -782,6 +784,7 @@ static void mt7915_txo_worker(void *wi_data, struct ieee80211_sta *sta)
 			       td->txbw, td->tx_rate_stbc, td->tx_rate_ldpc);
 }
 
+/* DEPRECATED: See vif_set_rate_override */
 static ssize_t mt7915_read_set_rate_override(struct file *file,
 					     char __user *user_buf,
 					     size_t count, loff_t *ppos)
@@ -828,6 +831,7 @@ static ssize_t mt7915_read_set_rate_override(struct file *file,
 }
 
 /* Set the rates for specific types of traffic.
+ * DEPRECATED: See vif_set_rate_override
  */
 static ssize_t mt7915_write_set_rate_override(struct file *file,
 					      const char __user *user_buf,
@@ -1007,6 +1011,7 @@ exit:
 	return ret;
 }
 
+/* DEPRECATED: See vif_set_rate_override */
 static const struct file_operations fops_set_rate_override = {
 	.read = mt7915_read_set_rate_override,
 	.write = mt7915_write_set_rate_override,
@@ -1660,8 +1665,11 @@ int mt7915_init_debugfs(struct mt7915_phy *phy)
 	}
 	debugfs_create_u32("ignore_radar", 0600, dir,
 			   &dev->ignore_radar);
+
+	/* DEPRECATED: See vif_set_rate_override */
 	debugfs_create_file("set_rate_override", 0600, dir,
 			    dev, &fops_set_rate_override);
+
 	debugfs_create_file("muru_onoff", 0600, dir, dev, &fops_muru_onoff);
 
 	if (!ext_phy)
@@ -1811,6 +1819,185 @@ void mt7915_sta_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 {
 	debugfs_create_file("fixed_rate", 0600, dir, sta, &fops_fixed_rate);
 	debugfs_create_file("hw-queues", 0400, dir, sta, &mt7915_queues_fops);
+}
+
+static ssize_t mt7915_vif_read_set_rate_override(struct file *file,
+					     char __user *user_buf,
+					     size_t count, loff_t *ppos)
+{
+	struct mt7915_vif *mvif = file->private_data;
+	struct mt7915_sta *msta = &mvif->sta;
+	struct mt76_testmode_data *td = &msta->test;
+
+	char *buf;
+	int size = 8000;
+	int rv;
+
+	const char help_text[] =
+		"This configures specific tx rate parameters for all DATA frames on a vdev\n"
+		"To set a value, you specify key-value pairs:\n"
+		"tpc=x sgi=x mcs=x nss=x pream=x retries=x dynbw=0|1 bw=x enable=0|1\n"
+		"  pream: 0=cck, 1=ofdm, 2=HT, 3=VHT, 4=HE_SU\n"
+		"  cck-mcs: 0=1Mbps, 1=2Mbps, 3=5.5Mbps, 3=11Mbps\n"
+		"  ofdm-mcs: 0=6Mbps, 1=9Mbps, 2=12Mbps, 3=18Mbps, 4=24Mbps, 5=36Mbps,"
+		" 6=48Mbps, 7=54Mbps\n"
+		"  sgi: HT/VHT: 0 | 1, HE 0: 1xLTF+0.8us, 1: 2xLTF+0.8us, 2: 2xLTF+1.6us,"
+		" 3: 4xLTF+3.2us, 4: 4xLTF+0.8us\n"
+		"  tpc: adjust power from defaults, in 1/2 db units 0 - 31, 16 is default\n"
+		"  bw is 0-3 for 20-160\n"
+		"  stbc: 0 off, 1 on\n"
+		"  ldpc: 0 off, 1 on\n"
+		"For example:\n"
+		"echo \"tpc=255 sgi=1 mcs=0 nss=1 pream=3 retries=1 dynbw=0 bw=0"
+		" active=1\" > set_rate_override\n";
+
+	buf = kzalloc(size, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	size = scnprintf(buf, size,
+			 "%s\nactive=%d tpc=%d sgi=%d mcs=%d nss=%d"
+			 " pream=%d retries=%d dynbw=%d bw=%d stbc=%d ldpc=%d\n",
+			 help_text,
+			 td->txo_active, td->tx_power[0],
+			 td->tx_rate_sgi, td->tx_rate_idx,
+			 td->tx_rate_nss, td->tx_rate_mode,
+			 td->tx_xmit_count, td->tx_dynbw,
+			 td->txbw, td->tx_rate_stbc, td->tx_rate_ldpc);
+
+	rv = simple_read_from_buffer(user_buf, count, ppos, buf, size);
+	kfree(buf);
+	return rv;
+}
+
+static void mt7915_vif_set_rate_override_worker(void *data, struct ieee80211_sta *sta)
+{
+	struct mt7915_sta *msta = (struct mt7915_sta *)sta->drv_priv;
+	struct mt76_testmode_data *td = data;
+
+	mt76_copy_rate_overrides(&msta->test, td);
+}
+
+/* Set the rates for specific types of traffic.
+ */
+static ssize_t mt7915_vif_write_set_rate_override(struct file *file,
+						  const char __user *user_buf,
+						  size_t count, loff_t *ppos)
+{
+	struct mt7915_vif *mvif = file->private_data;
+	struct mt7915_sta *msta = &mvif->sta;
+	struct mt76_testmode_data *td = NULL;
+	struct mt76_dev *dev = mvif->phy->mt76->dev;
+	struct ieee80211_vif *vif = mt7915_mvif_to_vif(mvif);
+	struct wireless_dev *wdev = ieee80211_vif_to_wdev(vif);
+
+	char buf[180] = { 0 };
+	char tmp[20];
+	char *tok;
+	int ret;
+	long rc;
+
+	simple_write_to_buffer(buf, sizeof(buf) - 1, ppos, user_buf, count);
+
+#define MT7915_VIF_PARSE_LTOK(a, b)							\
+	do {									\
+		tok = strstr(buf, #a "=");					\
+		if (tok) {							\
+			char *tspace;						\
+			strncpy(tmp, tok + strlen(#a "="), sizeof(tmp) - 1);	\
+			tmp[sizeof(tmp) - 1] = 0;				\
+			tspace = strstr(tmp, " ");				\
+			if (tspace)						\
+				*tspace = 0;					\
+			if (kstrtol(tmp, 0, &rc) != 0)				\
+				dev_info(dev->dev,				\
+					 "mt7915: set-rate-override: " #a	\
+					 "= could not be parsed, tmp: %s\n",	\
+					 tmp);					\
+			else							\
+				td->b = rc;					\
+		}								\
+	} while (0)
+
+	/* drop the possible '\n' from the end */
+	if (buf[count - 1] == '\n')
+		buf[count - 1] = '\0';
+
+	mutex_lock(&dev->mutex);
+
+	/* Ignore empty lines, 'echo' appends them sometimes at least. */
+	if (buf[0] == '\0') {
+		ret = count;
+		goto exit;
+	}
+
+	td = &msta->test;
+
+	MT7915_VIF_PARSE_LTOK(tpc, tx_power[0]);
+	MT7915_VIF_PARSE_LTOK(sgi, tx_rate_sgi);
+	MT7915_VIF_PARSE_LTOK(mcs, tx_rate_idx);
+	MT7915_VIF_PARSE_LTOK(nss, tx_rate_nss);
+	MT7915_VIF_PARSE_LTOK(pream, tx_rate_mode);
+	MT7915_VIF_PARSE_LTOK(retries, tx_xmit_count);
+	MT7915_VIF_PARSE_LTOK(dynbw, tx_dynbw);
+	MT7915_VIF_PARSE_LTOK(ldpc, tx_rate_ldpc);
+	MT7915_VIF_PARSE_LTOK(stbc, tx_rate_stbc);
+	MT7915_VIF_PARSE_LTOK(bw, txbw);
+	MT7915_VIF_PARSE_LTOK(active, txo_active);
+
+	/* To match Intel's API
+	 * HE 0: 1xLTF+0.8us, 1: 2xLTF+0.8us, 2: 2xLTF+1.6us, 3: 4xLTF+3.2us, 4: 4xLTF+0.8us
+	 */
+	if (td->tx_rate_mode >= 4) {
+		if (td->tx_rate_sgi == 0) {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 0;
+		} else if (td->tx_rate_sgi == 1) {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 1;
+		} else if (td->tx_rate_sgi == 2) {
+			td->tx_rate_sgi = 1;
+			td->tx_ltf = 1;
+		} else if (td->tx_rate_sgi == 3) {
+			td->tx_rate_sgi = 2;
+			td->tx_ltf = 2;
+		} else {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 2;
+		}
+	}
+
+	ieee80211_iterate_stations_atomic(dev->hw, mt7915_vif_set_rate_override_worker, td);
+
+	dev_info(dev->dev,
+		 "mt7915: set-rate-overrides, vdev %s active=%d tpc=%d sgi=%d ltf=%d mcs=%d"
+		 " nss=%d pream=%d retries=%d dynbw=%d bw=%d ldpc=%d stbc=%d\n",
+		 wdev->netdev->name,
+		 td->txo_active, td->tx_power[0], td->tx_rate_sgi, td->tx_ltf, td->tx_rate_idx,
+		 td->tx_rate_nss, td->tx_rate_mode, td->tx_xmit_count, td->tx_dynbw,
+		 td->txbw, td->tx_rate_ldpc, td->tx_rate_stbc);
+
+	ret = count;
+
+exit:
+	mutex_unlock(&dev->mutex);
+	return ret;
+}
+
+static const struct file_operations fops_vif_set_rate_override = {
+	.read = mt7915_vif_read_set_rate_override,
+	.write = mt7915_vif_write_set_rate_override,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+void mt7915_vif_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif)
+{
+	struct mt7915_vif *mvif = (struct mt7915_vif *)vif->drv_priv;
+
+	debugfs_create_file("set_rate_override", 0600, vif->debugfs_dir, mvif,
+			    &fops_vif_set_rate_override);
 }
 
 #endif

K900/linux

drivers/net/wireless/mediatek/mt76/mt7915/debugfs.c
@@ -751,12 +751,14 @@ mt7915_fw_util_wa_show(struct seq_file *file, void *data)
 
 DEFINE_SHOW_ATTRIBUTE(mt7915_fw_util_wa);
 
+/* DEPRECATED: See vif_set_rate_override */
 struct mt7915_txo_worker_info {
 	char *buf;
 	int sofar;
 	int size;
 };
 
+/* DEPRECATED: See vif_set_rate_override */
 static void mt7915_txo_worker(void *wi_data, struct ieee80211_sta *sta)
 {
 	struct mt7915_txo_worker_info *wi = wi_data;
@@ -782,6 +784,7 @@ static void mt7915_txo_worker(void *wi_data, struct ieee80211_sta *sta)
 			       td->txbw, td->tx_rate_stbc, td->tx_rate_ldpc);
 }
 
+/* DEPRECATED: See vif_set_rate_override */
 static ssize_t mt7915_read_set_rate_override(struct file *file,
 					     char __user *user_buf,
 					     size_t count, loff_t *ppos)
@@ -828,6 +831,7 @@ static ssize_t mt7915_read_set_rate_override(struct file *file,
 }
 
 /* Set the rates for specific types of traffic.
+ * DEPRECATED: See vif_set_rate_override
  */
 static ssize_t mt7915_write_set_rate_override(struct file *file,
 					      const char __user *user_buf,
@@ -1007,6 +1011,7 @@ exit:
 	return ret;
 }
 
+/* DEPRECATED: See vif_set_rate_override */
 static const struct file_operations fops_set_rate_override = {
 	.read = mt7915_read_set_rate_override,
 	.write = mt7915_write_set_rate_override,
@@ -1725,8 +1730,11 @@ int mt7915_init_debugfs(struct mt7915_phy *phy)
 	}
 	debugfs_create_u32("ignore_radar", 0600, dir,
 			   &dev->ignore_radar);
+
+	/* DEPRECATED: See vif_set_rate_override */
 	debugfs_create_file("set_rate_override", 0600, dir,
 			    dev, &fops_set_rate_override);
+
 	debugfs_create_file("muru_onoff", 0600, dir, dev, &fops_muru_onoff);
 
 	if (!ext_phy)
@@ -1876,6 +1884,185 @@ void mt7915_sta_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 {
 	debugfs_create_file("fixed_rate", 0600, dir, sta, &fops_fixed_rate);
 	debugfs_create_file("hw-queues", 0400, dir, sta, &mt7915_queues_fops);
+}
+
+static ssize_t mt7915_vif_read_set_rate_override(struct file *file,
+					     char __user *user_buf,
+					     size_t count, loff_t *ppos)
+{
+	struct mt7915_vif *mvif = file->private_data;
+	struct mt7915_sta *msta = &mvif->sta;
+	struct mt76_testmode_data *td = &msta->test;
+
+	char *buf;
+	int size = 8000;
+	int rv;
+
+	const char help_text[] =
+		"This configures specific tx rate parameters for all DATA frames on a vdev\n"
+		"To set a value, you specify key-value pairs:\n"
+		"tpc=x sgi=x mcs=x nss=x pream=x retries=x dynbw=0|1 bw=x enable=0|1\n"
+		"  pream: 0=cck, 1=ofdm, 2=HT, 3=VHT, 4=HE_SU\n"
+		"  cck-mcs: 0=1Mbps, 1=2Mbps, 3=5.5Mbps, 3=11Mbps\n"
+		"  ofdm-mcs: 0=6Mbps, 1=9Mbps, 2=12Mbps, 3=18Mbps, 4=24Mbps, 5=36Mbps,"
+		" 6=48Mbps, 7=54Mbps\n"
+		"  sgi: HT/VHT: 0 | 1, HE 0: 1xLTF+0.8us, 1: 2xLTF+0.8us, 2: 2xLTF+1.6us,"
+		" 3: 4xLTF+3.2us, 4: 4xLTF+0.8us\n"
+		"  tpc: adjust power from defaults, in 1/2 db units 0 - 31, 16 is default\n"
+		"  bw is 0-3 for 20-160\n"
+		"  stbc: 0 off, 1 on\n"
+		"  ldpc: 0 off, 1 on\n"
+		"For example:\n"
+		"echo \"tpc=255 sgi=1 mcs=0 nss=1 pream=3 retries=1 dynbw=0 bw=0"
+		" active=1\" > set_rate_override\n";
+
+	buf = kzalloc(size, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	size = scnprintf(buf, size,
+			 "%s\nactive=%d tpc=%d sgi=%d mcs=%d nss=%d"
+			 " pream=%d retries=%d dynbw=%d bw=%d stbc=%d ldpc=%d\n",
+			 help_text,
+			 td->txo_active, td->tx_power[0],
+			 td->tx_rate_sgi, td->tx_rate_idx,
+			 td->tx_rate_nss, td->tx_rate_mode,
+			 td->tx_xmit_count, td->tx_dynbw,
+			 td->txbw, td->tx_rate_stbc, td->tx_rate_ldpc);
+
+	rv = simple_read_from_buffer(user_buf, count, ppos, buf, size);
+	kfree(buf);
+	return rv;
+}
+
+static void mt7915_vif_set_rate_override_worker(void *data, struct ieee80211_sta *sta)
+{
+	struct mt7915_sta *msta = (struct mt7915_sta *)sta->drv_priv;
+	struct mt76_testmode_data *td = data;
+
+	mt76_copy_rate_overrides(&msta->test, td);
+}
+
+/* Set the rates for specific types of traffic.
+ */
+static ssize_t mt7915_vif_write_set_rate_override(struct file *file,
+						  const char __user *user_buf,
+						  size_t count, loff_t *ppos)
+{
+	struct mt7915_vif *mvif = file->private_data;
+	struct mt7915_sta *msta = &mvif->sta;
+	struct mt76_testmode_data *td = NULL;
+	struct mt76_dev *dev = mvif->phy->mt76->dev;
+	struct ieee80211_vif *vif = mt7915_mvif_to_vif(mvif);
+	struct wireless_dev *wdev = ieee80211_vif_to_wdev(vif);
+
+	char buf[180] = { 0 };
+	char tmp[20];
+	char *tok;
+	int ret;
+	long rc;
+
+	simple_write_to_buffer(buf, sizeof(buf) - 1, ppos, user_buf, count);
+
+#define MT7915_VIF_PARSE_LTOK(a, b)							\
+	do {									\
+		tok = strstr(buf, #a "=");					\
+		if (tok) {							\
+			char *tspace;						\
+			strscpy(tmp, tok + strlen(#a "="), sizeof(tmp) - 1);	\
+			tmp[sizeof(tmp) - 1] = 0;				\
+			tspace = strstr(tmp, " ");				\
+			if (tspace)						\
+				*tspace = 0;					\
+			if (kstrtol(tmp, 0, &rc) != 0)				\
+				dev_info(dev->dev,				\
+					 "mt7915: set-rate-override: " #a	\
+					 "= could not be parsed, tmp: %s\n",	\
+					 tmp);					\
+			else							\
+				td->b = rc;					\
+		}								\
+	} while (0)
+
+	/* drop the possible '\n' from the end */
+	if (buf[count - 1] == '\n')
+		buf[count - 1] = '\0';
+
+	mutex_lock(&dev->mutex);
+
+	/* Ignore empty lines, 'echo' appends them sometimes at least. */
+	if (buf[0] == '\0') {
+		ret = count;
+		goto exit;
+	}
+
+	td = &msta->test;
+
+	MT7915_VIF_PARSE_LTOK(tpc, tx_power[0]);
+	MT7915_VIF_PARSE_LTOK(sgi, tx_rate_sgi);
+	MT7915_VIF_PARSE_LTOK(mcs, tx_rate_idx);
+	MT7915_VIF_PARSE_LTOK(nss, tx_rate_nss);
+	MT7915_VIF_PARSE_LTOK(pream, tx_rate_mode);
+	MT7915_VIF_PARSE_LTOK(retries, tx_xmit_count);
+	MT7915_VIF_PARSE_LTOK(dynbw, tx_dynbw);
+	MT7915_VIF_PARSE_LTOK(ldpc, tx_rate_ldpc);
+	MT7915_VIF_PARSE_LTOK(stbc, tx_rate_stbc);
+	MT7915_VIF_PARSE_LTOK(bw, txbw);
+	MT7915_VIF_PARSE_LTOK(active, txo_active);
+
+	/* To match Intel's API
+	 * HE 0: 1xLTF+0.8us, 1: 2xLTF+0.8us, 2: 2xLTF+1.6us, 3: 4xLTF+3.2us, 4: 4xLTF+0.8us
+	 */
+	if (td->tx_rate_mode >= 4) {
+		if (td->tx_rate_sgi == 0) {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 0;
+		} else if (td->tx_rate_sgi == 1) {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 1;
+		} else if (td->tx_rate_sgi == 2) {
+			td->tx_rate_sgi = 1;
+			td->tx_ltf = 1;
+		} else if (td->tx_rate_sgi == 3) {
+			td->tx_rate_sgi = 2;
+			td->tx_ltf = 2;
+		} else {
+			td->tx_rate_sgi = 0;
+			td->tx_ltf = 2;
+		}
+	}
+
+	ieee80211_iterate_stations_atomic(dev->hw, mt7915_vif_set_rate_override_worker, td);
+
+	dev_info(dev->dev,
+		 "mt7915: set-rate-overrides, vdev %s active=%d tpc=%d sgi=%d ltf=%d mcs=%d"
+		 " nss=%d pream=%d retries=%d dynbw=%d bw=%d ldpc=%d stbc=%d\n",
+		 wdev->netdev->name,
+		 td->txo_active, td->tx_power[0], td->tx_rate_sgi, td->tx_ltf, td->tx_rate_idx,
+		 td->tx_rate_nss, td->tx_rate_mode, td->tx_xmit_count, td->tx_dynbw,
+		 td->txbw, td->tx_rate_ldpc, td->tx_rate_stbc);
+
+	ret = count;
+
+exit:
+	mutex_unlock(&dev->mutex);
+	return ret;
+}
+
+static const struct file_operations fops_vif_set_rate_override = {
+	.read = mt7915_vif_read_set_rate_override,
+	.write = mt7915_vif_write_set_rate_override,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+void mt7915_vif_add_debugfs(struct ieee80211_hw *hw, struct ieee80211_vif *vif)
+{
+	struct mt7915_vif *mvif = (struct mt7915_vif *)vif->drv_priv;
+
+	debugfs_create_file("set_rate_override", 0600, vif->debugfs_dir, mvif,
+			    &fops_vif_set_rate_override);
 }
 
 #endif